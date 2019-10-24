5c7a0901ba305e086247774d956ae50e96e15d21
jawnwa22 <jawnwa22@gmail.com>
2019-10-24 09:17:31 +0800

    feat: 新增页面主题组件

---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    div#main
+        nav-drawer
+        //- tool-bar
+        v-content
+            v-container(fluid)
+                keep-alive
+                    router-view
+</template>
+
+<script>
+import ToolBar from "@/components/Toolbar.vue";
+import NavDrawer from "@/components/Navdrawer/Navdrawer.vue";
+export default {
+    components: {
+        ToolBar,
+        NavDrawer
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#main {
+    height: 100%;
+}
+</style>
