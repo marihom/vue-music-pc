482c50accebbb3acc2838b656d3e75a9e1ff4e34
jawnwa22 <jawnwa22@gmail.com>
2019-10-24 09:18:19 +0800

    feat: 新增歌曲库页面

---
+++ src/views/Library/Library.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+    div.library-page
+        v-app-bar(app)
+            v-toolbar-title My Library
+            template(v-slot:extension)
+                v-tabs(align-with-title v-model="tab")
+                    v-tab Songs
+                    v-tab Tab2
+                    v-tab Tab3
+        v-tabs-items(v-model="tab")
+            v-tab-item
+                Songs
+</template>
+
+<script>
+import ToolBar from "@/components/Toolbar.vue";
+import Songs from "./components/Songs.vue";
+export default {
+    components: {
+        ToolBar,
+        Songs
+    },
+    data() {
+        return {
+            tab: null,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.library-page {
+    height: 100%;
+}
+</style>
---
+++ src/views/Library/components/Songs.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+    div.songs-tab
+        v-simple-table
+            template(v-slot:default)
+                tbody
+                    tr(v-for="item in desserts" :keys="item.name")
+                        td {{ item.name }}
+                        td {{ item.calories }}
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            desserts: [
+                {
+                    name: "Frozen Yogurt",
+                    calories: 159
+                },
+                {
+                    name: "Ice cream sandwich",
+                    calories: 237
+                },
+                {
+                    name: "Eclair",
+                    calories: 262
+                },
+                {
+                    name: "Cupcake",
+                    calories: 305
+                },
+                {
+                    name: "Gingerbread",
+                    calories: 356
+                },
+                {
+                    name: "Jelly bean",
+                    calories: 375
+                },
+                {
+                    name: "Lollipop",
+                    calories: 392
+                },
+                {
+                    name: "Honeycomb",
+                    calories: 408
+                },
+                {
+                    name: "Donut",
+                    calories: 452
+                },
+                {
+                    name: "KitKat",
+                    calories: 518
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.songs-tab {
+    height: 100%;
+}
+</style>
