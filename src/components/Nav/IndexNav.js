9497426459d46fd66354be2f684f9e04dd8e4258
jawnwa22 <jawnwa22@gmail.com>
2019-11-06 21:33:01 +0800

    feat(音乐主页面)：新增左侧导航配置文件

---
+++ src/components/Nav/IndexNav.js
@@ -0,0 +1,16 @@
+/**
+ * @description: 主页面左侧导航栏
+ * @since: 2019-10-23 20:19:09
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-10-24 23:40:02
+ */
+const nav = [
+    {
+        title: "My Library",
+        icon: "library_music",
+        to: "/library"
+    }
+];
+
+export default nav;
