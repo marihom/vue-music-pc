c96ec74d290c2cd324c35b77fa7b87b59983855a
jawnwa22 <jawnwa22@gmail.com>
2019-11-06 21:31:55 +0800

    feat(上传)：管理后台新增上传页面

---
+++ src/views/admin/song/upload.vue
@@ -0,0 +1,10 @@
+<!--
+ * @description: 上传歌曲文件页面
+ * @since: 2019-11-01 00:23:23
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-11-01 00:28:03
+ -->
+<template lang="pug">
+    div hellow
+</template>
