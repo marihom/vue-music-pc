3f37c49acd44147cb14c7d522bb135a5dba148be
jawnwa22 <jawnwa22@gmail.com>
2019-11-06 21:32:33 +0800

    feat(管理后台)：新增左侧导航配置文件

---
+++ src/components/Nav/AdminNav.js
@@ -0,0 +1,21 @@
+/**
+ * @description: 管理页面导航栏
+ * @since: 2019-10-24 23:39:36
+ * @Author: jawnwa22
+ * @LastEditors: jawnwa22
+ * @LastEditTime: 2019-11-01 00:34:22
+ */
+const nav = [
+    {
+        title: '歌库',
+        icon: 'view_list',
+        to: '/song/list'
+    },
+    {
+        title: '上传文件',
+        icon: 'cloud_upload',
+        to: '/song/upload'
+    }
+];
+
+export default nav;
